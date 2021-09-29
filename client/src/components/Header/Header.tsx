540959582cc9753c1a431db69cf59ddc7efa1c0c
kelvinlien <Mr.Secret0987@gmail.com>
2021-09-29 20:19:45 +0700

    [HcEeRQ7V] - Add styling for header

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
 import { Image,Box } from "@chakra-ui/react";
 import React from "react";
 import CatLogo from "../../assets/CatwikiLogo.svg";
+import "./Header.css"
 export default function Header(){
     return (
-      <Box boxSize="sm" bg="" color="white">
-        <Image src={CatLogo} alt="Cat header logo" boxSize="150px"/>
+      <Box boxSize="sm">
+        <Image src={CatLogo} alt="Cat header logo" class="headerLogo"/>
       </Box>
     );
 }
