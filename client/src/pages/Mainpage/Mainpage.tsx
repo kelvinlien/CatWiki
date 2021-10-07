2c7cff027d9aa3e145b1faa0ba1e443c0807e3b7
kelvinlien <Mr.Secret0987@gmail.com>
2021-10-07 21:24:44 +0700

    [HcEeRQ7V] - Improve Header and Searchbar, replace Stack with Container in Mainpage

--- client/src/pages/Mainpage/Mainpage.tsx
+++ client/src/pages/Mainpage/Mainpage.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import HeroImage from "../../components/HeroImage/HeroImage";
-import {Stack, Text} from "@chakra-ui/react";
-import ImagesRow from "../../components/ImagesRow/ImagesRow";
+import {Container, Text} from "@chakra-ui/react";
 import CatWikiLogo from "../../components/CatWikiLogo/CatWikiLogo";
 import "./Mainpage.css"
 import SearchBar from "../../components/SearchBar/SearchBar";
@@ -10,12 +9,12 @@ import DiscoveryImage from "../../components/DiscoveryImage/DiscoveryImage";
 const images = ["https://via.placeholder.com/1200x800","https://via.placeholder.com/1200x800","https://via.placeholder.com/1200x800","https://via.placeholder.com/1200x800"];
 export default function Mainpage(){
     return(
-        <Stack>
+        <Container>
             <HeroImage/>
             <CatWikiLogo parentClass="heroCatWikiLogo"/>
             <Text className={"heroText"}>Get to know more about your cat breed</Text>
             <SearchBar placeholder={"Enter your breed"} parentClass={"heroSearchBar"}/>
             <DiscoveryImage/>
-        </Stack>
+        </Container>
     );
 }
