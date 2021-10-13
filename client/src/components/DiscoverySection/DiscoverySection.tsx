68c42903bf01cfa0305e9a793a6aee5f47248ffd
kelvinlien <Mr.Secret0987@gmail.com>
2021-10-13 22:43:08 +0700

    [HcEeRQ7V] - Add DiscoveryIntroductionSection

--- client/src/components/DiscoverySection/DiscoverySection.tsx
+++ client/src/components/DiscoverySection/DiscoverySection.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import {Box, Center, Text} from "@chakra-ui/react";
+import {Box, Center, HStack, Text} from "@chakra-ui/react";
 import "./DiscoverySection.css"
+import {ArrowForwardIcon} from "@chakra-ui/icons";
+import ImagesRow from "../ImagesRow/ImagesRow";
 
 function DiscoverySection(props: any) {
     return (
         <Center>
-            <Box className={"discoverySectionWrapper"}>
-                <Text className={"mostSearchText"}>Most Searched Breeds</Text>
-                <Box className={"mostSearchDivider"}>
-                    <hr/>
+            <Box className={"discoverySectionBackground"}>
+                <Box className={"discoverySectionWrapper"}>
+                    <Text className={"mostSearchText"}>Most Searched Breeds</Text>
+                    <Box className={"mostSearchDivider"}>
+                        <hr/>
+                    </Box>
+                    <HStack className={"discoveryIntroductionSection"} justify={"space-between"}>
+                        <Text className={"discoveryIntroductionText"}>66+ Breeds For you to discover</Text>
+                        <HStack spacing={6}>
+                            <Text className={"seeMoreText"}>SEE MORE</Text>
+                            <ArrowForwardIcon className={"seeMoreIcon"}/>
+                        </HStack>
+                    </HStack>
+                    {/*<ImagesRow/>*/}
                 </Box>
             </Box>
         </Center>
