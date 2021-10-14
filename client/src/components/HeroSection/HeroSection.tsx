571eac1ba8ead65ccc4addd690fc5e32eed348c7
kelvinlien <Mr.Secret0987@gmail.com>
2021-10-14 21:19:01 +0700

    [HcEeRQ7V] - Use background image in HeroSection, remove HeroImage, improve styling

--- client/src/components/HeroSection/HeroSection.tsx
+++ client/src/components/HeroSection/HeroSection.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import HeroImage from "../HeroImage/HeroImage";
 import CatWikiLogo from "../CatWikiLogo/CatWikiLogo";
-import {Box, Text} from "@chakra-ui/react";
+import {Box, Center, Text} from "@chakra-ui/react";
 import SearchBar from "../SearchBar/SearchBar";
 import "./HeroSection.css"
 
 function HeroSection(props: any) {
     return (
-        <Box className={"heroSectionWrapper"}>
-            <HeroImage/>
-            <CatWikiLogo parentClass="heroCatWikiLogo"/>
-            <Text className={"heroText"}>Get to know more about your cat breed</Text>
-            <SearchBar placeholder={"Enter your breed"} parentClass={"heroSearchBar"}/>
-        </Box>
+        <Center>
+            <Box className={"heroSectionBackground"}>
+                <Box className={"heroSectionWrapper"}>
+                    <CatWikiLogo parentClass="heroCatWikiLogo"/>
+                    <Text className={"heroText"}>Get to know more about your cat breed</Text>
+                    <SearchBar placeholder={"Enter your breed"} parentClass={"heroSearchBar"}/>
+                </Box>
+            </Box>
+        </Center>
     );
 }
 
