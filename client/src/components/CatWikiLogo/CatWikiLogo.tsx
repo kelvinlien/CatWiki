c5bb2dbf439d5222756adf0d0e5a14893682d1d0
kelvinlien <Mr.Secret0987@gmail.com>
2021-10-20 21:10:02 +0700

    [uqeZHWBc] - Make Header responsive, CatWikiLogo receives styling from parent

--- client/src/components/CatWikiLogo/CatWikiLogo.tsx
+++ client/src/components/CatWikiLogo/CatWikiLogo.tsx
@@ -1,10 +1,9 @@
 import {Image} from "@chakra-ui/react";
 import React from "react";
 import CatLogo from "../../assets/CatwikiLogo.svg";
-import "./CatWikiLogo.css"
 
 export default function CatWikiLogo(props: any){
     return (
-        <Image src={CatLogo} alt="Cat header logo" className={`logo ${props.parentClass ? props.parentClass : ""}`}/>
+        <Image src={CatLogo} alt="Cat header logo" className={`${props.parentClass ? props.parentClass : ""}`}/>
     );
 }
