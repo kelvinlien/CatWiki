2eb3845725aecd890e6c5ab80948d2b0bb6ac23d
kelvinlien <Mr.Secret0987@gmail.com>
2021-10-21 21:54:12 +0700

    [uqeZHWBc] - Responsive Footer v1.0

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,14 +1,18 @@
-import {Text, HStack} from "@chakra-ui/react";
+import {Text, HStack, Wrap, WrapItem} from "@chakra-ui/react";
 import React from "react";
 import "./Footer.css"
 import CatWikiLogo from "../CatWikiLogo/CatWikiLogo";
 export default function Footer(){
     return (
-      <HStack className="footerWrapper" justify={"space-between"}>
-        <CatWikiLogo parentClass="footerLogo"/>
-          <Text className={"footerCredit"}>
-              © created by <strong>Winston</strong> - devChallenge.io 2021
-          </Text>
-      </HStack>
+      <Wrap className="footerWrapper" justify={"space-between"}>
+          <WrapItem>
+              <CatWikiLogo parentClass="footerLogo"/>
+          </WrapItem>
+          <WrapItem>
+              <Text className={"footerCredit"}>
+                  © created by <strong>Winston</strong> - devChallenge.io 2021
+              </Text>
+          </WrapItem>
+      </Wrap>
     );
 }
